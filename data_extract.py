cfc3165921b10d3a5e81a01c2ee92e78519cc7a1
GSylph <sheranidanish12@gmail.com>
2025-04-02 02:37:40 +0530

    Data Preprocessing: Created Function to process training files

--- data_extract.py
+++ data_extract.py
@@ -11,33 +11,34 @@ def xz_files_in_dir(directory):
         if filename.endswith('.xz') and os.path.isfile(os.path.join(directory, filename)):
             files.append(filename)
     return files
+
+
 folder_path = "C:/Users/hp/Desktop/Code/Projects/fcc-gpt/openwebtext"
-output_path="output{}.txt"
-vocab_path="vocab.txt"
+output_file_train="output_train.txt"
+output_file_val="output_val.txt"
+vocab_file="vocab.txt"
 split_files = int(input("How many files do you want to split into?"))
 
 files = xz_files_in_dir(folder_path)
 total_files = len(files)
-max_count = total_files // split_files if split_files != 0 else total_files
 
-print(f"Total files: {total_files}")
-print(f"Files to split into: {split_files}")
-print(f"Max count per split: {max_count}")
+#calculate the split indices
+split_index=int(total_files*0.9) #90% for training, 10% for validation
+files_train = files[:split_index]
+files_val = files[split_index:]
 
+#Process the files for training and validation separately 
 vocab = set()
 
-for i in range(split_files):
-    with open(output_path.format(i), 'wb') as outfile:
-        for count,filename in enumerate(tqdm(files[:max_count],total=max_count)):
-            if count>=max_count:
-                break
-            file_path = os.path.join(folder_path, filename)
-            with lzma.open(file_path, 'rt', encoding="utf-8") as infile:
-                data = infile.read()
-                outfile.write(data.encode('utf-8'))
-                vocab.update(data)
-        files = files[max_count:]
 
-with open(vocab_path, 'w',encoding = "utf-8") as vfile:
-    for char in vocab:
-        vfile.write(char + '\n')
+#Process the training files
+with open(output_file_train, 'w', encoding='utf-8') as outfile:
+    for filename in tqdm(files_train, total=len(files_train)):
+        file_path = os.path.join(folder_path, filename)
+        with lzma.open(file_path, 'rt', encoding='utf-8') as infile:
+            for line in infile:
+                text=infile.read()
+                outfile.write(line)
+                characters = set(text)
+                vocab.update(characters)
+
