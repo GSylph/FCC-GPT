91f07a80eb22f8200526cd407022f5a9bdfbce05
GSylph <sheranidanish12@gmail.com>
2025-04-15 21:47:12 +0530

    Porting code to script

--- chatbot.py
+++ chatbot.py
@@ -38,34 +38,7 @@ int_to_string= {i:ch for i,ch in enumerate(chars)}
 encode=lambda s: [string_to_int[c] for c in s]
 decode=lambda l: ''.join([int_to_string[i] for i in l])
 
-#Memory mapping for using small snippets of text from single file of any size
-def get_random_chunk(split):
-    filename= "train_split.txt" if split == 'train' else "val_split.txt"
-    with open(filename, 'rb') as f:
-        with mmap.mmap(f.fileno(),0, access=mmap.ACCESS_READ) as mm:
-            #Determine the file size and a random position to start reading
-            file_size = len(mm)
-            start_pos = random.randint(0, (file_size)-block_size*batch_size)
 
-            #Seek to the random position and read a block of text
-            mm.seek(start_pos)
-            block= mm.read(block_size*batch_size-1)
-            
-            #Decode the block to a string, ignoring any invalid byte sequences
-            decoded_block = block.decode('utf-8', errors='ignore').replace('\r','')
-
-            #Train and test splits
-            data= torch.tensor(encode(decoded_block), dtype=torch.long)
-    return data
-
-
-def get_batch(split):
-    data= get_random_chunk(split)
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i + block_size] for i in ix])
-    y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
-    x, y = x.to(device), y.to(device)
-    return x, y
 
 
 class Head(nn.Module):
@@ -191,36 +164,8 @@ with open('model-01.pkl', 'rb') as f:
 print('Loaded Successfully')
 m=model.to(device)
 
-@torch.no_grad()
-def estimate_loss():
-    out = {}
-    model.eval()
-    for split in  ['train','val']:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            x, y = get_batch(split)
-            logits, loss = model(x, y)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    model.train()
-    return out
-
-#Pytorch optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
-for iter in range(max_iters):
-    if iter % eval_iters == 0:
-        losses = estimate_loss()
-        print(f"Iter {iter}: train loss: {losses['train']:.3f}, val loss: {losses['val']:.3f}")
-
-    xb, yb = get_batch('train')
-
-    logits, loss = model.forward(xb, yb)
-
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward() 
-    optimizer.step()
-print(loss.item())
-
-with open('model-01.pkl','wb') as f:
-    pickle.dump(model,f)
-print('Model saved!')
+while True:
+    prompt=input("Enter your prompt:\n")
+    context= torch.tensor(encode(prompt), dtype=torch.long,device=device) #context is a tensor of zeros of size (1,block_size)
+    generated_chars= decode(m.generate(context.unsqueeze(0), max_new_tokens=150)[0].tolist()) #generate 150 new tokens
+    print(f'Completion : \n {generated_chars}')
